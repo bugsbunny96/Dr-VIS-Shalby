a1429202edf35904272af00cbeb4d607951b1249
Rishabh Sharma <b.rishu.pandit@gmail.com>
2022-09-30 11:01:02 +0530

    all done

--- assets/app.js
+++ assets/app.js
@@ -40,12 +40,23 @@ const carousels = document.querySelectorAll('[data-carousel]');
 carousels.forEach(setUpCarousel);
 // carousel
 
-// Accordion
-const accordion = document.getElementsByClassName('container');
+// -------------------++++++++++++++++++++++++++++------------------------
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+// accordion
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
 }
-// Accordion
+// accordion
